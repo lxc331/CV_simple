fbb7fc161b54989ae8f590acbe80a272c0be1797
marvel <1722247182@qq.com>
2026-03-30 10:00:41 +0800

    test server connection

--- pytorch_test.py
+++ pytorch_test.py
@@ -1,3 +1,8 @@
 import torch
+import numpy
 
-print(torch.__version__)
+print("torch version:", torch.__version__)
+print("numpy version:", numpy.__version__)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+print(torch.cuda.is_available())
